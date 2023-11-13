47574325f989f86618bea7099037d8f9720ee6c1
Ivan Dagelić <dagelic.ivan@gmail.com>
2023-11-13 11:43:25 +0100

    Update daytona.rb

--- daytona.rb
+++ daytona.rb
@@ -1,19 +1,20 @@
 class Daytona < Formula
   desc "Daytona CLI"
   homepage "https://daytona.io"
+  version "8.53.6"
 
   # Construct the URL based on the user's OS and ARCH
   if OS.mac? && Hardware::CPU.arm?
-    url "https://download.daytona.io/latest/daytona-darwin-arm64"
+    url "https://download.daytona.io/v#{version}/daytona-darwin-arm64"
     sha256 "<<DARWIN_ARM64_SHA256>>"
   elsif OS.mac? && Hardware::CPU.intel?    
-    url "https://download.daytona.io/latest/daytona-darwin-amd64"    
+    url "https://download.daytona.io/v#{version}/daytona-darwin-amd64"    
     sha256 "<<DARWIN_AMD64_SHA256>>"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://download.daytona.io/latest/daytona-linux-amd64"
+    url "https://download.daytona.io/v#{version}/daytona-linux-amd64"
     sha256 "dccdd06919c0db60b01f7a911e91fe2bd96c8ec6561cd326b2eb87ca553095f3"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://download.daytona.io/latest/daytona-linux-arm64"
+    url "https://download.daytona.io/v#{version}/daytona-linux-arm64"
     sha256 "<<DARWIN_AMD64_SHA256>>"
   else
     odie "Unsupported OS/ARCH combination"
